42fba97a04052f692bea78faeca1443820df4bc9
abdullah apu <apu@mac.home>
2025-01-19 17:36:29 -0500

    adding two complex number

---
+++ src/Java_01_BasicPrograms/Java_01_4_addTwoComplex.java
@@ -0,0 +1,37 @@
+package Java_01_BasicPrograms;
+
+public class Java_01_4_addTwoComplex {
+
+    public static void main(String[] args) {
+        ComplexNumber n1 = new ComplexNumber(4, 5);
+        ComplexNumber n2 = new ComplexNumber(10, 5);
+
+        System.out.print("First complex number: ");
+        System.out.println(n1.showC());
+        System.out.print("Second complex number: ");
+        System.out.println(n2.showC());
+
+        ComplexNumber res = ComplexNumber.addC(n1, n2);
+        System.out.print("result: ");
+        System.out.println(res.showC());
+    }
+}
+
+class ComplexNumber{
+    int real, image;
+    ComplexNumber(int real, int image){
+        this.real = real;
+        this.image = image;
+    }
+    public String showC(){
+        return this.real + "+i" + this.image;
+    }
+    public static ComplexNumber addC(ComplexNumber n1, ComplexNumber n2){
+        ComplexNumber res = new ComplexNumber(0, 0);
+        res.real = n1.real + n2.real;
+        res.image = n2.image + n2.image;
+        return res;
+    }
+
+
+}
