1152cabf479cddd7ea9941dfabe66e84398ae5c2
abdullah apu <apu@mac.home>
2025-01-20 03:51:08 -0500

    check leap year

---
+++ src/Java_01_BasicPrograms/Java_01_09_checkLeapYear.java
@@ -0,0 +1,21 @@
+package Java_01_BasicPrograms;
+import java.util.Scanner;
+public class Java_01_09_checkLeapYear {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the year: ");
+        int year = sc.nextInt();
+        boolean isLeap = false;
+        if(year % 4 == 0){
+            isLeap = true;
+            if(year % 100 == 0){
+                isLeap = year % 400 == 0;
+            }
+        }
+        if(isLeap){
+            System.out.println(year + " is a leap year!");
+        }else {
+            System.out.println(year + " is not a leap year!");
+        }
+    }
+}
