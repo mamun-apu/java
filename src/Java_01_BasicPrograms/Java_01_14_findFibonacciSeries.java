201dfc389cb18f3164a9ba8193346770dd7e5db4
abdullah apu <apu@mac.home>
2025-01-21 01:17:34 -0500

    fib

--- src/Java_01_BasicPrograms/Java_01_14_findFibonacciSeries.java
+++ src/Java_01_BasicPrograms/Java_01_14_findFibonacciSeries.java
@@ -1,4 +1,30 @@
 package Java_01_BasicPrograms;
 
-public class Java_01_14_findFebonacciSeries {
+import java.util.Scanner;
+
+public class Java_01_14_findFibonacciSeries {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter the number: ");
+        int N = sc.nextInt();
+
+        int[] arr = new int[2*N+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for(int i = 2; i <= N*2; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        for(int x: arr){
+            System.out.print(x + ", ");
+        }
+        System.out.println();
+        int sum = 0;
+        for(int i = 0; i <= N*2; i+=2){
+            sum+=arr[i];
+            System.out.print(arr[i] + ", ");
+        }
+        System.out.println();
+        System.out.println("Sum: " + sum);
+    }
 }
