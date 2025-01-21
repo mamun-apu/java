02b9b7932c4037dddc02cf22fa53c36acf3ff24c
abdullah apu <apu@mac.home>
2025-01-21 18:20:02 -0500

    rectanglePerimeter

--- src/Java_01_BasicPrograms/Java_01_17_rectanglePerimeter.java
+++ src/Java_01_BasicPrograms/Java_01_17_rectanglePerimeter.java
@@ -1,5 +1,18 @@
 package Java_01_BasicPrograms;
 
-public class Java_01_17_rectanglePerimeter {
+import java.util.Scanner;
 
+public class Java_01_17_rectanglePerimeter {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter length L: ");
+        double l = sc.nextDouble();
+
+        System.out.print("enter width W: ");
+        double w = sc.nextDouble();
+
+        double rectangle = 2*(l+w);
+        System.out.println("the rectangle is: " + rectangle);
+    }
 }
