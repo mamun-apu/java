054f1f258fef608188c979b7460533a827b98cd4
abdullah apu <apu@mac.home>
2025-01-27 02:53:51 -0500

    octaldecimal

--- src/Java_03_ConversionProgram/Java_03_02_octalAndDecimal.java
+++ src/Java_03_ConversionProgram/Java_03_02_octalAndDecimal.java
@@ -2,7 +2,7 @@ package Java_03_ConversionProgram;
 
 public class Java_03_02_octalAndDecimal {
     public static void main(String[] args){
-//        String octalNum = "3223";
+        String octalNum = "3223";
 //        int num = Integer.parseInt(octalNum, 8);
 //        System.out.println(num);
 //        System.out.println(Integer.parseInt("3223", 8) );
@@ -10,6 +10,9 @@ public class Java_03_02_octalAndDecimal {
         System.out.println(octalToDecimal(3224));
         int result = octalToDecimal(3224);
         System.out.println(decimalToOctal(result));
+        System.out.println(((Object) octalNum).getClass().getSimpleName());
+        System.out.println(((Object) result).getClass().getSimpleName());
+
     }
     public static int octalToDecimal(int octal){
         int result = 0;
--- src/Java_Object/JavaObject.java
+++ src/Java_Object/JavaObject.java
@@ -1,7 +1,16 @@
 package Java_Object;
 
 public class JavaObject{
-    public static void main(String[] args) {
+    public static void main(String args[])
+    {
 
+        String onum = "157";
+
+
+        int num = Integer.parseInt(onum, 8);
+
+        System.out.println(
+                "Decimal equivalent of Octal value 157 is: "
+                        + num);
     }
 }
