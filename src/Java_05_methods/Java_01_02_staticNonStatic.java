6d3e084021ea892a1d93dabd8a0d861b53fdcade
abdullah apu <apu@mac.home>
2025-01-30 06:17:43 -0500

    static non static

---
+++ src/Java_05_methods/Java_01_02_staticNonStatic.java
@@ -0,0 +1,36 @@
+package Java_05_methods;
+
+/*
+    A static method is a method that belongs to a class, but it does not
+    belong to an instance of that class and this method can be called
+    without the instance or object of that class. Every method in java
+    defaults to a non-static method without static keyword proceeding it.
+    Non-static methods can access any static method and static variable,
+    without creating an instance of the object
+
+*/
+
+public class Java_01_02_staticNonStatic {
+}
+class Helper {
+
+    // Static method
+    public static int sum(int a, int b)
+    {
+        // Simply returning the sum
+        return a + b;
+    }
+    public static void main(String[] args)
+    {
+        // Custom values for integer
+        // to be summed up
+        int n = 3, m = 6;
+
+        // Calling the static method of above class
+        // and storing sum in integer variable
+        int s = Helper.sum(n, m);
+
+        // Print and display the sum
+        System.out.print("sum is = " + s);
+    }
+}
