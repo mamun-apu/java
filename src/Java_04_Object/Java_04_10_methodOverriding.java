ecf55735093e35b03f06891733929be1d2143666
abdullah apu <apu@mac.home>
2025-01-30 05:49:54 -0500

    method overriding

---
+++ src/Java_04_Object/Java_04_10_methodOverriding.java
@@ -0,0 +1,36 @@
+package Java_04_Object;
+
+public class Java_04_10_methodOverriding {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.move();
+        d.eat();
+        d.bark();
+    }
+}
+/*
+    Overriding in Java occurs when a subclass implements a
+    method which is already defined in the superclass or
+    Base Class. The method in the subclass must have the
+    same signature as in the superclass. It allows the subclass
+    to modify the inherited methods.
+ */
+
+class Animal{
+    void move(){
+        System.out.println("Animal is moving");
+    }
+    void eat(){
+        System.out.println("Animal is eating");
+    }
+}
+class Dog extends Animal{
+    @Override
+    void move(){
+        System.out.println("Dog is running");
+    }
+    void bark(){
+        System.out.println("Dog is barking.");
+    }
+
+}
