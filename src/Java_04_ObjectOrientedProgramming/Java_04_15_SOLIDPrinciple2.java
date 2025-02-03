c1ef1e3f0766468c926c0deb77ff83b254c8bb28
Abdullah Al Mamun Apu <abmamun504@gmail.com>
2025-02-02 22:55:13 -0500

    srp and ocp

---
+++ src/Java_04_ObjectOrientedProgramming/Java_04_15_SOLIDPrinciple2.java
@@ -0,0 +1,83 @@
+package Java_04_ObjectOrientedProgramming;
+
+import javax.management.Notification;
+
+public class Java_04_15_SOLIDPrinciple2 {
+    public static void main(String[] args) {
+        NotificationService s = new SendOTP();
+        s.sendNotification("hello");
+    }
+}
+
+
+/*
+    SRP-> A class can only be changed by one stakeholder or a group of stakeholder. Only
+    those function will remain, without whom the class will not be able to function properly
+*/
+// Breaking SRP...
+class UserUpdate{
+    String getMessage(){ // related to database
+        return "";
+    }
+    void sendMessage(){ // related to internet
+
+    }
+}
+
+// Follows SRP...
+class GetInfo{
+    String getMessage(){
+        return "";
+    }
+}
+
+class SendInfo{
+    void sendMessage(){
+        //...
+    }
+}
+
+// OCP -> Open for extension and Closed for modification. editing is not the problem.
+//  The problem is -> being able to edit.
+
+// It is editable.
+class SendInfoo{
+    void sendMessage(String str){
+        //...
+    }
+    void SendEmail(String str){
+        //...
+    }
+    void sendOTP(String str){
+        //...
+    }
+    //we can add more function in here. that is the problem
+}
+
+//Here is the solution.
+interface NotificationService{
+    void sendNotification(String message);
+}
+
+class SendEmail implements NotificationService {
+    @Override
+    public void sendNotification(String message) {
+        System.out.println("message");
+    }
+}
+
+class SendOTP implements NotificationService{
+    @Override
+    public void sendNotification(String message) {
+        System.out.println("message");
+    }
+}
+class UseCaseNotification{
+    public void use(NotificationService service){
+        service = new SendOTP();
+        service.sendNotification("hello");
+
+    }
+}
+
+
