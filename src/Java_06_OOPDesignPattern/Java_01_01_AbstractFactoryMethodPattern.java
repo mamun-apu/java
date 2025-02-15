698bbc03dcaf30750b1fc1d8d901d4ceec16d7f6
Abdullah Al Mamun Apu <abmamun504@gmail.com>
2025-02-15 13:20:12 -0500

    abstract factory pattern

---
+++ src/Java_06_OOPDesignPattern/Java_01_01_AbstractFactoryMethodPattern.java
@@ -0,0 +1,135 @@
+package Java_06_OOPDesignPattern;
+
+public class Java_01_01_AbstractFactoryMethodPattern {
+    public static void main(String[] args){
+
+    }
+}
+/*-----Interfaces-------*/
+interface Button{
+    void showButton();
+}
+interface Checkbox{
+    void showCheckbox();
+}
+interface Menu{
+    void showMenu();
+}
+
+interface GUIFactory{
+    Button createButton();
+    Checkbox createCheckbox();
+    Menu createMenu();
+}
+
+// -------Concrete classes-------
+
+// -------Concrete Button-------
+class WindowsButton implements Button{
+    @Override
+    public void showButton(){
+        System.out.println("Win button");
+    }
+}
+class MacButton implements Button{
+    @Override
+    public void showButton(){
+        System.out.println("Mac button");
+    }
+}
+class LinuxButton implements Button{
+    @Override
+    public void showButton(){
+        System.out.println("Linux button!");
+    }
+}
+
+// -------Concrete Checkbox-------
+class WindowsCheckbox implements Checkbox{
+    @Override
+    public void showCheckbox(){
+        System.out.println("Windows checkbox");
+    }
+}
+class MacCheckbox implements Checkbox{
+    @Override
+    public void showCheckbox(){
+        System.out.println("Mac checkbox");
+    }
+}
+class LinuxCheckbox implements Checkbox{
+    @Override
+    public void showCheckbox(){
+        System.out.println("Linux checkbox");
+    }
+}
+
+// -------Concrete Menu-------
+class WindowsMenu implements Menu{
+    @Override
+    public void showMenu(){
+        System.out.println("Windows Menu");
+    }
+}
+
+class MacMenu implements Menu{
+    @Override
+    public void showMenu(){
+        System.out.println("Mac menu");
+    }
+}
+class LinuxMenu implements Menu{
+    @Override
+    public void showMenu(){
+        System.out.println("Linux menu");
+    }
+}
+//Concrete factory
+class WindowsFactory implements GUIFactory{
+    @Override
+    public Button createButton(){
+        return new WindowsButton();
+    }
+
+    @Override
+    public Checkbox createCheckbox(){
+        return new WindowsCheckbox();
+    }
+
+    @Override
+    public Menu createMenu(){
+        return new WindowsMenu();
+    }
+}
+class MacFactory implements GUIFactory{
+    @Override
+    public Button createButton(){
+        return new MacButton();
+    }
+
+    @Override
+    public Checkbox createCheckbox(){
+        return new MacCheckbox();
+    }
+    @Override
+    public Menu createMenu(){
+        return new MacMenu();
+    }
+
+}
+class LinuxFactory implements GUIFactory{
+    @Override
+    public Button createButton(){
+        return new LinuxButton();
+    }
+
+    @Override
+    public Checkbox createCheckbox(){
+        return new LinuxCheckbox();
+    }
+
+    @Override
+    public Menu createMenu(){
+        return new LinuxMenu();
+    }
+}
